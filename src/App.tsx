9332ed4de7597908b12a5603a7c49ef2e8169ac3
이은주 <dldmswn071918@gmail.com>
2023-10-25 12:12:05 +0900

    테마,택배사 리스트  국내/국외 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,126 @@
+import { useEffect , useState } from "react";
+
+interface Company {
+  International : string;
+  Code : string;
+  Name : string
+}
+
+interface ThemeColor {
+  [key : string] : {
+    back : string;
+    hover : string;
+    active : string;
+    text : string
+  }
+}
+
+interface ButtonType {
+  name : string;
+  theme : string;
+}
+
+
 function App() {
+
+  const [carriers, setCarriers] = useState<Company[]>([]);
+  // usestate 타입 설정하기 useState<string>(); 필터되어서 보여짐,필터후
+  const [allCarriers,setAllCarriers] = useState<Company[]>([]);
+  // 모든 데이터 넣어둔걸 유지하고 필터된것은 위에서 국내,국외로 할거임,필터될때
+  const [theme , setTheme] = useState<string>('default');
+  // 객체안에 객체가 들어갈거임
+
+  const [tcode,setTcode] = useState<string>('04');
+  // 택배코드
+  const [tinvoice,setTinvoice] = useState<string>('');
+  // 실제 운송장 번호라서 빈값
+  const [tname,setTname] = useState<string>('CJ대한통운');
+  const [isBtn,setIsBtn] = useState<number | null>(null);
+  // const [isBtn,setIsBtn] = useState<number>();
+
+  const themeColor : ThemeColor = {
+    "default" : {
+      "back" : "bg-[#03588c]",
+      "hover" : "hover:bg-[#0477be]",
+      "active" : "bg-[#0468a5]",
+      "text" : "text-[#03588c]"
+    },
+    "pink" : {
+      "back" : "bg-[#fc7283]",
+      "hover" : "hover:bg-[#fda4af]",
+      "active" : "bg-[#fc8b99]",
+      "text" : "text-[#fc7283]"
+    },
+    "skyblue" : {
+      "back" : "bg-[#38bdf8]",
+      "hover" : "hover:bg-[#69cdfa]",
+      "active" : "bg-[#51c5f9]",
+      "text" : "text-[#38bdf8]"
+    }
+  }
+
+  const buttons : ButtonType[] = [
+    {name : "기본" , theme : "default"},
+    {name : "핑크" , theme : "pink"},
+    {name : "스카이블루" , theme : "skyblue"}
+  ]
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const res = await fetch(`http://info.sweettracker.co.kr/api/v1/companylist?t_key=${process.env.REACT_APP_API_KEY}`)
+        const data = await res.json();
+        // console.log(data)
+        setCarriers(data.Company);
+        setAllCarriers(data.Company)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[])
+
+  const selectCode = (BtnNumber : number , code : string , name : string) => {
+    setIsBtn(BtnNumber)
+    setTcode(code)
+    setTname(name);
+    const isInternational = BtnNumber === 2 ? 'true' : 'false';
+    const filterCarriers = allCarriers.filter(e => e.International === isInternational);
+    setCarriers(filterCarriers)
+  }
+
   return (
    <>
-    <p className="text-center bg-orange-200">Lorem ipsum dolor sit amet.</p>
+    <div className={`p-5 text-black text-sm md:text-xl xl:text-2xl flex justify-between ${themeColor[theme].back}`}>
+      <h3 className="font-extrabold">국내.외 택배조회 시스템</h3>
+      <div>
+        <span>테마 : </span>
+        {
+          buttons.map((e,i)=>{
+            return(
+              <button onClick={()=>setTheme(e.theme)} key={i} className="mx-1 md:mx-2 xl:mx-3">{e.name}</button>
+            )
+          })
+        }
+      </div>
+    </div>
+    <div className="border-b basis-full py-2 px-2 flex justify-center items-center text-sm">
+      <span className="bais-[30%] text-center mr-5">국내 / 국외 선택</span>
+      <button className={`text-sm border p-1 px-5 rounded hover:text-white mr-4 ${isBtn === 1 ? 'text-white' : 'text-black'} ${themeColor[theme].hover} ${isBtn === 1 ? themeColor[theme].active : ''}`} onClick={()=> selectCode(1,'04','CJ대한통운')}>국내</button>
+      <button className={`text-sm border p-1 px-5 rounded hover:text-white ${isBtn === 2 ? 'text-white' : 'text-black'} ${themeColor[theme].hover} ${isBtn === 2 ? themeColor[theme].active : ''}`} onClick={()=> selectCode(2,'12','EMS')}>국외</button>
+    </div>
+    {/* {tcode}{tname} */}
+    <select value={tcode} onChange={(e)=>{setTcode(e.target.value)}}>
+      {
+        carriers.map((e,i)=>{
+          return(
+            <option key={i} value={e.Code}>
+              {e.Name}
+            </option> 
+          )
+        })
+      }
+    </select>
    </>
   );
 }
